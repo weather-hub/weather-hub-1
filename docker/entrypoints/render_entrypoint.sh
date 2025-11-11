4dfbf9ffb1d3e9937c63ef8feafeb301095a97bf
miguelmir123 <miguelmirceballos@gmail.com>
2025-11-11 10:04:24 +0100

    fix: seeders on pre-production

--- docker/entrypoints/render_entrypoint.sh
+++ docker/entrypoints/render_entrypoint.sh
@@ -3,9 +3,9 @@
 # ---------------------------------------------------------------------------
 # Creative Commons CC BY 4.0 - David Romero - Diverso Lab
 # ---------------------------------------------------------------------------
-# This script is licensed under the Creative Commons Attribution 4.0 
-# International License. You are free to share and adapt the material 
-# as long as appropriate credit is given, a link to the license is provided, 
+# This script is licensed under the Creative Commons Attribution 4.0
+# International License. You are free to share and adapt the material
+# as long as appropriate credit is given, a link to the license is provided,
 # and you indicate if changes were made.
 #
 # For more details, visit:
@@ -24,7 +24,7 @@ fi
 
 # Check if the database is empty
 if [ $(mariadb -u $MARIADB_USER -p$MARIADB_PASSWORD -h $MARIADB_HOSTNAME -P $MARIADB_PORT -D $MARIADB_DATABASE -sse "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MARIADB_DATABASE';") -eq 0 ]; then
- 
+
     echo "Empty database, migrating..."
 
     # Get the latest migration revision
@@ -34,6 +34,7 @@ if [ $(mariadb -u $MARIADB_USER -p$MARIADB_PASSWORD -h $MARIADB_HOSTNAME -P $MAR
 
     # Run the migration process to apply all database schema changes
     flask db upgrade
+    rosemary db:seed
 
 else
 
@@ -41,7 +42,7 @@ else
 
     # Get the current revision to avoid duplicate stamp
     CURRENT_REVISION=$(mariadb -u $MARIADB_USER -p$MARIADB_PASSWORD -h $MARIADB_HOSTNAME -P $MARIADB_PORT -D $MARIADB_DATABASE -sse "SELECT version_num FROM alembic_version LIMIT 1;")
-    
+
     if [ -z "$CURRENT_REVISION" ]; then
         # If no current revision, stamp with the latest revision
         flask db stamp head
@@ -49,6 +50,7 @@ else
 
     # Run the migration process to apply all database schema changes
     flask db upgrade
+    rosemary db:seed
 fi
 
 # Start the application using Gunicorn, binding it to port 80
