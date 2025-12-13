702f8c95fda4a559c4edf106186f639eaedf6a71
miguelmir123 <miguelmirceballos@gmail.com>
2025-12-13 14:41:06 +0100

    fix: comments and feedbacks on datasets work

--- app/modules/comments/forms.py
+++ app/modules/comments/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class CommentForm(FlaskForm):
-    content = TextAreaField("Comment", validators=[DataRequired()])
+
+    content = TextAreaField(
+        "Comment",
+        validators=[
+            DataRequired(),
+            Length(max=50, message="If you want to leave a comment, please write less than 50 characters. Otherwise, use feedbacks.")
+        ]
+    )
     submit = SubmitField("Submit")
